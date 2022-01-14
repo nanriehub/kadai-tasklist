69f9b97314ca289a91811a70bbe6e78b0212b087
nanriehub <d50deko@yahoo.co.jp>
2022-01-14 13:44:29 +0000

    [Modify]tasks_controller indent, add form partial

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,57 +1,59 @@
 class TasksController < ApplicationController
- def index 
-     @tasks = Task.all
- end
- 
- def show
-     @task = Task.find(params[:id])
- end
- 
- def new
-     @task = Task.new
- end
- 
- def create
-     @task = Task.new(task_params)
-  
-     if @task.save
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+
+    if @task.save
       flash[:success] = 'Task が正常に投稿されました'
       redirect_to @task
-     else
+    else
       flash.now[:danger] = 'Task が投稿されませんでした'
       render :new
-   end
- end
- 
- def edit
-     @task = Task.find(params[:id])
- end
- 
- def update
-     @task = Task.find(params[:id])
-   
-     if @task.update(task_params)
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    set_task
+    if @task.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
       redirect_to @task
-     else
+    else
       flash.now[:danger] = 'Task は更新されませんでした'
       render :edit
-      flash[:success] = 'Task は正常に削除されました'
-   end
+    end
   end
  
- def destroy
-     @task = Task.find(params[:id])
-     @task.destroy
-     redirect_to tasks_url
- end
- 
- private
- 
- #StrongParameter
- 
- def task_params
-  params.require(:task).permit(:content)
- end
-end
+  def destroy
+    @task.destroy
 
+    flash[:success] = 'Task は正常に削除されました'
+    redirect_to tasks_url
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  #StrongParameter
+ 
+  def task_params
+    params.require(:task).permit(:content)
+  end
+end

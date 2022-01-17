2185c9e3e1231005f13063d9d61d0f86e24c3197
nanriehub <d50deko@yahoo.co.jp>
2022-01-17 12:16:23 +0000

    add column kadai

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -53,6 +53,6 @@ class TasksController < ApplicationController
   #StrongParameter
  
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content, :status)
   end
 end

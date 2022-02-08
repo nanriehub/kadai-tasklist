ae4f62584561ce7c9dec52165de23827e428d1db
nanriehub <d50deko@yahoo.co.jp>
2022-02-08 12:46:28 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'users/create'
     root to: 'tasks#index'
     
     resources :tasks
     
+    get 'signup', to: 'users#new'#/signup
+    resources :users, only: [:create]
     
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
